6a6f6521337ffab468ee853d4fd5b5e5d825c73b
Kaichi-Nakajima <kaichinakajima@gmail.com>
2025-01-23 23:23:35 -0800

    Add files via upload

---
+++ nhl_databse.sql
@@ -0,0 +1,16 @@
+--
+-- File generated with SQLiteStudio v3.4.13 on Thu Jan 23 23:21:32 2025
+--
+-- Text encoding used: UTF-8
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: gamelog_2020
+CREATE TABLE IF NOT EXISTS gamelog_2020 (
+    Date [DATE TIME DATETIME VISITOR TEXT VISITOR_G INTEGER HOME TEXT HOME_G INTEGER PAST_REGULATION TEXT]
+);
+
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
