fe8fed9e903dbda396c956745704f5cab219a283
Kaichi-Nakajima <kaichinakajima@gmail.com>
2025-02-19 22:31:28 -0800

    Update gamelogs

--- nhl_databse.sql
+++ nhl_databse.sql
@@ -65,6 +65,40 @@ CREATE TABLE IF NOT EXISTS gamelogs (
     fiveOnFiveGoalsAgainst     INTEGER
 );
 
+-- inserting data into table
+INSERT INTO gamelogs 
+(Team,Season,gameId,OpposingTeam,gameDate,home_or_away,situation, corsiPercentage,
+fenwickPercentage, xGoalsFor,shotsOnGoalFor,blockedShotAttemptsFor, goalsFor, savedShotsOnGoalFor, penaltyMinutesFor,
+faceOffsWonFor,hitsFor,takeawaysFor, giveawaysFor, lowDangerShotsFor, mediumDangerShotsFor, highDangerShotsFor,
+lowDangerxGoalsFor, mediumDangerxGoalsFor,highDangerxGoalsFor, lowDangerGoalsFor, mediumDangerGoalsFor,
+highDangerGoalsFor,xGoalsAgainst,shotsOnGoalAgainst,blockedShotAttemptsAgainst,goalsAgainst,savedShotsOnGoalAgainst, penaltyMinutesAgainst,faceOffsWonAgainst, 
+hitsAgainst,takeawaysAgainst,giveawaysAgainst, lowDangerShotsAgainst, mediumDangerShotsAgainst, highDangerShotsAgainst, lowDangerxGoalsAgainst,
+mediumDangerxGoalsAgainst, highDangerxGoalsAgainst, lowDangerGoalsAgainst, mediumDangerGoalsAgainst, highDangerGoalsAgainst)
+SELECT
+column1, column2,column4, column6, column7, column8, column10,column12, column13, column16, column25, column27, 
+column29, column36, column39,column40, column41,column42,column43,column44,column45,column46,column47,column48,
+column49,column50,column51,column52, column64, column73,column75, column77, column84, column87,column88,
+column89, column90,column91,column92,column93,column94,column95,column96,column97,column98,column99,column100
+FROM all_teams_logs
+
+-- add columns for other situations
+ALTER TABLE gamelogs
+    ADD shortHandedxGoalsFor FLOAT;
+
+ALTER TABLE gamelogs
+    ADD shortHandedxGoalsAgainst FLOAT;
+
+ALTER TABLE gamelogs
+    ADD powerPlayxGoalsFor FLOAT;
+
+ALTER TABLE gamelogs
+    ADD powerPlayxGoalsAgainst FLOAT;
+
+ALTER TABLE gamelogs
+    ADD otherxGoalsFor FLOAT;
+
+ALTER TABLE gamelogs
+    ADD otherxGoalsAgainst FLOAT;
 
 COMMIT TRANSACTION;
 PRAGMA foreign_keys = on;
